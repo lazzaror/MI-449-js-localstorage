10ea81f87ebe0841ced055aab2fc6fcec81876cb
unknown <rachel.lazzaro7@gmail.com>
2020-04-02 15:17:20 -0400

    successfully made page remember theme

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+/* Page Theme localStorage Reminder */
+function loadTheme() {
+  if (window.localStorage.getItem('pageTheme') === null) {
+    window.localStorage.setItem('pageTheme', 'day-time')
+  }
 
+  document.body.setAttribute('class', window.localStorage.getItem('pageTheme'))
+
+}
+
+/* Page Views Counter */
 var numVisits = 0
 
 if (window.localStorage.getItem('numVisits') === null) {
@@ -10,13 +20,16 @@ window.localStorage.setItem('numVisits', numVisits)
 
 document.getElementById('pgViews').innerHTML = numVisits
 
+/* Toggle Button */
 document.getElementById('btn1').addEventListener('click', changeTime())
 
-function changeTime () {
+function changeTime() {
   if (document.body.className === 'day-time') {
     document.body.setAttribute('class', 'night-time')
+    window.localStorage.setItem('pageTheme', 'night-time')
   }
   else {
     document.body.setAttribute('class', 'day-time')
+    window.localStorage.setItem('pageTheme', 'day-time')
   }
 }
