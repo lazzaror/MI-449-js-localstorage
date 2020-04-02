027e9654fc1b6f88ff0a0af908bc384163f4c759
unknown <rachel.lazzaro7@gmail.com>
2020-04-02 14:36:21 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var numVisits = 0
+
+if (window.localStorage.getItem('numVisits') === null) {
+  window.localStorage.setItem('numVisits', 0)
+}
+
+numVisits = parseInt(window.localStorage.getItem('numVisits')) + 1
+window.localStorage.setItem('numVisits', numVisits)
+
+document.getElementById('pgViews').setAttribute('body', numVisits)
+
+document.getElementById('btn1').addEventListener('click', function () {
+  if (document.body.className === 'day-time') {
+    document.body.setAttribute('class', 'night-time')
+  }
+  else {
+    document.body.setAttribute('class', 'day-time')
+  }
+})
